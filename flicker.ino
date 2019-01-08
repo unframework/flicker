bd3768e01266509e3fb9fce7818809d0751de3f4
Nick Matantsev <nick.matantsev@gmail.com>
2019-01-07 22:14:08 -0500

    Naive coop multitasking

--- flicker.ino
+++ flicker.ino
@@ -16,24 +16,52 @@ const unsigned long cycleLength = 5000; // in microseconds; 5000us is 200Hz
 
 class PinState {
     int pin;
-    double slice;
+    double yOffset;
     int level = 100;
     unsigned long nextCycleStart;
     bool currentState = false;
 
 public:
-    PinState(int pin, double slice);
-    void update(unsigned long currentMicros);
+    PinState(int pin, double phase);
+    void computeLevel();
+    void render(unsigned long currentMicros);
 };
 
-PinState::PinState(int pin, double slice) {
+PinState::PinState(int pin, double phase) {
     this->pin = pin;
-    this->slice = slice;
+    this->yOffset = phase * 100.0;
 
-    nextCycleStart = slice * 100; // sort of randomize phase
+    nextCycleStart = phase * cycleLength; // stagger computation/strobe
 }
 
-void PinState::update(unsigned long currentMicros) {
+void PinState::computeLevel() {
+    // change PWM level as needed
+    double currentSeconds = micros() / 1000000.0;
+
+    // slow overall brightness change
+    double baseGlow = 0.5 + 0.5 * sn.noise(currentSeconds * 0.03, yOffset + 0);
+    baseGlow *= baseGlow; // exaggerate the dips in brightness
+
+    renderAll();
+
+    // simple frequent oscillation
+    double baseBreath = 0.5 + 0.5 * sn.noise(currentSeconds * 0.5, yOffset + 0.5);
+
+    renderAll();
+
+    // amplify and clamp the throttle noise for a clearer mode
+    double flickerThrottleRaw = sn.noise(currentSeconds * 0.5, yOffset + 1.0);
+    renderAll();
+    double flickerThrottle = min(1.0, 3.0 * max(0.0, flickerThrottleRaw - 1.0 + 0.5) / 0.5);
+    double flicker = 0.5 + 0.3 * sn.noise(currentSeconds * 8.0, yOffset + 2.0) + 0.2 * sn.noise(currentSeconds * 14.0, yOffset + 2.0);
+    renderAll();
+
+    // mix up the proportioned factors
+    double total = baseGlow * (1.0 - baseBreath * 0.2) * (1.0 - flickerThrottle * flicker * 0.5);
+    level = total * cycleLength;
+}
+
+void PinState::render(unsigned long currentMicros) {
     if (currentState) {
         // downstroke
         if (nextCycleStart + level <= currentMicros) {
@@ -43,25 +71,6 @@ void PinState::update(unsigned long currentMicros) {
             // flip pin
             digitalWrite(pin, LOW);
             currentState = false;
-
-            // change PWM level as needed
-            double currentSeconds = currentMicros / 1000000.0;
-
-            // slow overall brightness change
-            double baseGlow = 0.5 + 0.5 * sn.noise(currentSeconds * 0.03, slice + 0);
-            baseGlow *= baseGlow; // exaggerate the dips in brightness
-
-            // simple frequent oscillation
-            double baseBreath = 0.5 + 0.5 * sn.noise(currentSeconds * 0.5, slice + 0.5);
-
-            // amplify and clamp the throttle noise for a clearer mode
-            double flickerThrottleRaw = sn.noise(currentSeconds * 0.5, slice + 1.0);
-            double flickerThrottle = min(1.0, 3.0 * max(0.0, flickerThrottleRaw - 1.0 + 0.5) / 0.5);
-            double flicker = 0.5 + 0.3 * sn.noise(currentSeconds * 8.0, slice + 2.0) + 0.2 * sn.noise(currentSeconds * 14.0, slice + 2.0);
-
-            // mix up the proportioned factors
-            double total = baseGlow * (1.0 - baseBreath * 0.2) * (1.0 - flickerThrottle * flicker * 0.5);
-            level = total * cycleLength;
         }
     } else {
         // upstroke
@@ -73,13 +82,20 @@ void PinState::update(unsigned long currentMicros) {
     }
 }
 
-class PinState a(2, 0.0), b(3, 10.0), c(4, 20.0), d(5, 30.0);
+class PinState a(2, 0.0), b(3, 0.25), c(4, 0.5), d(5, 0.75);
 
 void loop() {
+    a.computeLevel();
+    // b.computeLevel();
+    // c.computeLevel();
+    // d.computeLevel();
+}
+
+void renderAll() {
     unsigned long currentMicros = micros();
 
-    a.update(currentMicros);
-    b.update(currentMicros);
-    c.update(currentMicros);
-    d.update(currentMicros);
+    a.render(currentMicros);
+    b.render(currentMicros);
+    c.render(currentMicros);
+    d.render(currentMicros);
 }
