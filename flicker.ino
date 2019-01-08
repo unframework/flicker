c6c77799cf2f1a64a0a77288ac17972a04f00119
Nick Matantsev <nick.matantsev@gmail.com>
2019-01-08 02:04:42 -0500

    Use timer-based PWM

--- flicker.ino
+++ flicker.ino
@@ -1,6 +1,11 @@
+#include <TimerOne.h>
 #include <SimplexNoise.h>
 
 SimplexNoise sn;
+
+const unsigned long cycleLength = 6000; // in microseconds; 5000us is 200Hz
+const unsigned long pollPeriod = 60; // in microseconds
+unsigned long currentMicros = 0;
 
 void setup() {
     // initialize pins as output
@@ -9,22 +14,24 @@ void setup() {
     pinMode(4, OUTPUT);
     pinMode(5, OUTPUT);
 
+    Timer1.initialize(pollPeriod); // allow for some precision
+    Timer1.attachInterrupt(renderAll);
+
     sn.init();
 }
-
-const unsigned long cycleLength = 5000; // in microseconds; 5000us is 200Hz
 
 class PinState {
     int pin;
     double yOffset;
-    int level = 100;
+    unsigned long level = cycleLength / 2;
     unsigned long nextCycleStart;
     bool currentState = false;
 
 public:
     PinState(int pin, double phase);
     void computeLevel();
-    void render(unsigned long currentMicros);
+    void setLevel(unsigned long level);
+    void render();
 };
 
 PinState::PinState(int pin, double phase) {
@@ -34,34 +41,38 @@ PinState::PinState(int pin, double phase) {
     nextCycleStart = phase * cycleLength; // stagger computation/strobe
 }
 
+void PinState::setLevel(unsigned long level) {
+    noInterrupts();
+    this->level = level;
+    interrupts();
+}
+
 void PinState::computeLevel() {
+    noInterrupts();
+    unsigned long currentMicrosSnapshot = currentMicros;
+    interrupts();
+
     // change PWM level as needed
-    double currentSeconds = micros() / 1000000.0;
+    double currentSeconds = currentMicrosSnapshot / 1000000.0;
 
     // slow overall brightness change
     double baseGlow = 0.5 + 0.5 * sn.noise(currentSeconds * 0.03, yOffset + 0);
     baseGlow *= baseGlow; // exaggerate the dips in brightness
 
-    renderAll();
-
     // simple frequent oscillation
     double baseBreath = 0.5 + 0.5 * sn.noise(currentSeconds * 0.5, yOffset + 0.5);
 
-    renderAll();
-
     // amplify and clamp the throttle noise for a clearer mode
     double flickerThrottleRaw = sn.noise(currentSeconds * 0.5, yOffset + 1.0);
-    renderAll();
     double flickerThrottle = min(1.0, 3.0 * max(0.0, flickerThrottleRaw - 1.0 + 0.5) / 0.5);
     double flicker = 0.5 + 0.3 * sn.noise(currentSeconds * 8.0, yOffset + 2.0) + 0.2 * sn.noise(currentSeconds * 14.0, yOffset + 2.0);
-    renderAll();
 
     // mix up the proportioned factors
     double total = baseGlow * (1.0 - baseBreath * 0.2) * (1.0 - flickerThrottle * flicker * 0.5);
-    level = total * cycleLength;
+    setLevel(total * cycleLength);
 }
 
-void PinState::render(unsigned long currentMicros) {
+void PinState::render() {
     if (currentState) {
         // downstroke
         if (nextCycleStart + level <= currentMicros) {
@@ -86,16 +97,18 @@ class PinState a(2, 0.0), b(3, 0.25), c(4, 0.5), d(5, 0.75);
 
 void loop() {
     a.computeLevel();
-    // b.computeLevel();
-    // c.computeLevel();
-    // d.computeLevel();
+    b.computeLevel();
+    c.computeLevel();
+    d.computeLevel();
+
+    delay(20);
 }
 
 void renderAll() {
-    unsigned long currentMicros = micros();
+    currentMicros += pollPeriod;
 
-    a.render(currentMicros);
-    b.render(currentMicros);
-    c.render(currentMicros);
-    d.render(currentMicros);
+    a.render();
+    b.render();
+    c.render();
+    d.render();
 }
